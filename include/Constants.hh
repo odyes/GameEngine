6612e29fc45764bdda63dd30480b174853d1b2c1
odyes <ale.odalism@gmail.com>
2021-10-12 14:10:52 -0700

    update

---
+++ include/Constants.hh
@@ -0,0 +1,8 @@
+const unsigned int WINDOW_WIDTH{800};
+const unsigned int WINDOW_HEIGHT{800};
+const char* GAME_NAME{"Game Engine"};
+const char* ASSETS_SPRITES{"assets/sprites/sprites.png"};
+const char* ASSETS_TILES{"assets/sprites/tiles.png"};
+const char* ASSETS_TILE_GROUP_1{"assets/tileGroups/map1.tg"};
+const char* ASSETS_FONT{"assets/fonts/ARCADECLASSIC.TTF"};
+const float GAME_SCALE{4.f};
